1c82e702528943a405932eb9d9bf9bc928865eb9
Enzo <nzomorvan@gmail.com>
2024-04-11 00:36:11 +0200

    feat(ci/cd): pipeline pour le déploiement vers la prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://heinezo.github.io',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   }), icon()]
